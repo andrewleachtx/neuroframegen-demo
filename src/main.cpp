8c269c085d019779034e0dab24e13e9cd81b8eca
lz-gh <zelunliu@tamu.edu>
2025-02-01 19:09:14 -0600

    Add opencv2 and dv-processing header

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+
 #include <dv-processing/core/core.hpp>
+#include <dv-processing/core/frame.hpp>
 #include <dv-processing/io/mono_camera_recording.hpp>
+
+#include <opencv2/highgui.hpp>
+
 using std::cout, std::endl;
 
 int main() {
