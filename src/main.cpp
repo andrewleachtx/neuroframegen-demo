cbba56cdd05c79d21b8beb6898552193777aef93
GMar25 <gagemariano@gmail.com>
2025-02-15 01:45:44 -0600

    Add partial in-depth notes/summary of paper

--- src/main.cpp
+++ src/main.cpp
@@ -13,7 +13,7 @@ const int FPS = 1;
 const float SPF = 1000.0f / FPS; // milliseconds
 
 const std::chrono::milliseconds WINDOW_LENGTH = 100ms;
-const float PERCENT_OPEN_SHUTTER = 0.5;
+const float PERCENT_OPEN_SHUTTER = 0.5; // TODO: Change to start stop (so not always at start of frame)
 
 // dv::EventStore binary_exposure(int percentage) {
 //     return nullptr;
@@ -71,3 +71,25 @@ int main() {
 
     return 0;
 }
+
+
+/*
+Abstract: nothing
+Intro:
+-1: event occurs when brightness of pixel undergoes a change that exceeds a preset threshold. Contains timestamp, location, polarity
+-2: stropboscopic effect and flutter shutter
+-3: frame generation and frame rate/exposure are very important for some algorithms
+-4: summary of paper (digital flutter shutter to control motion blur)
+Digital coded exposure to emulate the frame formation of a conventional camera:
+-1: traditional camera tradeoff of shutter speed and brightness
+-2:  frame length != shutter length
+-3: (prior works) flutter shutter and coded exposure implementations (for traditional cameras)
+-4: (prior works) coded aperture????
+-5: Approaches to forming frames. Longer windows = more motion blur. Also dynamic range is good (u is actually mew)
+-6: virtual shutter. Since each pixel is asynchronous and software processed can apply arbitrary exposure function
+-7: equation for summation of events
+-8: Frames equation in terms of frequency (thinking of basis of frequencies where shutter is emphasizing some elements)
+-9: Visualization of boxcar side lobes. Just pay attention to width of first oscillation (the rest is side lobes)
+-10: Problems with DV and jAER (accidently applying a box filter). Need to sum smarter
+Digital coded exposure as a virtual shutter for conventional frame formation wavelet digital coded exposure
+*/
